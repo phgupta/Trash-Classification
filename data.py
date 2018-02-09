39b68810a55d0bce99681f47b27c20838d89d024
JiayinYang <jynyang@ucdavis.edu>
2018-02-08 21:22:22 -0800

    Add files via upload

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 import glob
+from random import shuffle
 import keras
 from sklearn.model_selection import KFold
 from classifier import classifier
@@ -12,6 +13,7 @@ def readData():
         im=np.array(Image.open(filename))
         myclass.append(0)
         image_list.append(im)
+    
     for filename in glob.glob('dataset-resized/cardboard/*.jpg'): #assuming gif
         im=np.array(Image.open(filename))
         image_list.append(im)
@@ -26,11 +28,15 @@ def readData():
         myclass.append(3)
     
     myclass = keras.utils.to_categorical(np.asarray(myclass), num_classes=4)
+    c = list(zip(image_list, myclass))
+    shuffle(c)
+    image_list, myclass = zip(*c)
     return image_list, myclass
 
-
+arr = []
 [x,y] = readData()
 x = np.asarray(x)
+y = np.asarray(y)
 # pca
 cv = KFold(n_splits = 10, shuffle = True, random_state = 100)
 for index_train, index_test in cv.split(x):
@@ -38,7 +44,7 @@ for index_train, index_test in cv.split(x):
     testsize = index_test.size
     train_dataset = x[index_train]
     test_dataset = x[index_test]
-    train_labels = y[index_train] 
+    train_labels = y[index_train]
     test_labels = y[index_test]
     classifier(train_dataset, test_dataset, train_labels, test_labels, trainsize, testsize)
     
